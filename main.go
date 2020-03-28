b81304c18b9ef70af0a1999b1eac09a9b4679292
Chantal Neo <Chantal@hotmail.sg>
2020-03-29 01:17:18 +0800

    Embedding Structs

--- main.go
+++ main.go
@@ -5,14 +5,38 @@ import "fmt"
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
+}
+
+type contactInfo struct {
+	email   string
+	zipCode int
 }
 
 func main() {
-	alex := person{"Alex", "Anderson"}                       // This approach right here of kind of relying upon the order of definition of fields. This can be catastrophic if we go along and then accidentally swap the order of those two fields
-	chantal := person{firstName: "Chantal", lastName: "Neo"} // This approach is better, because it means that we can always change the order of fields in the definition up there
-	var jerry person                                         // Third approach of declaring a new struct in Go. At this point, Go would prepopulate the two different fields with zero value. Refer to point 2 for more information
+	alex := person{
+		"Alex",
+		"Anderson",
+		contactInfo{
+			"alex.anderson@golang.com",
+			123456,
+		},
+	} // This approach right here of kind of relying upon the order of definition of fields. This can be catastrophic if we go along and then accidentally swap the order of those two fields
+	chantal := person{
+		firstName: "Chantal",
+		lastName:  "Neo",
+		contact: contactInfo{
+			email:   "chantal.neo@golang.com",
+			zipCode: 5412177,
+		},
+	} // This approach is better, because it means that we can always change the order of fields in the definition up there
+	var jerry person // Third approach of declaring a new struct in Go. At this point, Go would prepopulate the different fields with zero value. Refer to point 2 for more information
 	jerry.firstName = "Jerry"
 	jerry.lastName = "Gan"
+	jerry.contact = contactInfo{
+		email:   "jerry.gan@golang.com",
+		zipCode: 5201314,
+	}
 
 	fmt.Println(alex)
 	fmt.Println(chantal)
