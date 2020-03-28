9134432f7be14d6cb57c5cb5570bec59d7a00895
Chantal Neo <Chantal@hotmail.sg>
2020-03-29 00:42:05 +0800

    Defining Structs

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+type person struct {
+	firstName string
+	lastName  string
+}
+
 func main() {
 
 }
+
+// Notes:
+// 1. Whenever we make a struct we have to first define all of the different properties that a struct might have
+//    We're going to provide this in some type of rule set to Go and then we can create a value that matches that type of structure definition.
