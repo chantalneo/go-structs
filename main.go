97215a2459b18f8fcaf30561440b45725f802bb2
Chantal Neo <Chantal@hotmail.sg>
2020-03-29 00:53:17 +0800

    Declaring Structs

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
+import "fmt"
+
 type person struct {
 	firstName string
 	lastName  string
 }
 
 func main() {
+	alex := person{"Alex", "Anderson"}                       // This approach right here of kind of relying upon the order of definition of fields. This can be catastrophic if we go along and then accidentally swap the order of those two fields
+	chantal := person{firstName: "Chantal", lastName: "Neo"} // This approach is better, because it means that we can always change the order of fields in the definition up there
 
+	fmt.Println(alex)
+	fmt.Println(chantal)
 }
 
 // Notes:
